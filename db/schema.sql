ce1e28a1cf89f2aa3e9dd724648a9db9987a76c0
tocoteron <tocoteron@gmail.com>
2025-06-14 09:15:53 +0900

    refactor db schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,11 +6,11 @@ CREATE TABLE youtube_channels (
 );
 
 CREATE TABLE youtube_playlists (
-    channel_id TEXT NOT NULL REFERENCES youtube_channels (channel_id),
     playlist_id TEXT PRIMARY KEY,
+    channel_id TEXT NOT NULL REFERENCES youtube_channels (channel_id),
     is_uploads BOOLEAN NOT NULL,
     title TEXT,
-    CHECK (
+    CONSTRAINT youtube_playlists_check CHECK (
         (is_uploads = true AND title IS NULL) OR
         (is_uploads = false AND title IS NOT NULL)
     )
