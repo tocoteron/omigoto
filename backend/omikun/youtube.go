343931a75e6567a9337d43b12528963b724add82
tocoteron <tocoteron@gmail.com>
2025-06-18 13:27:57 +0900

    add omikun YouTubeChannelIdentity

---
+++ backend/omikun/youtube.go
@@ -0,0 +1,11 @@
+package omikun
+
+type YouTubeChannelIdentity struct {
+	ID     string
+	Handle string
+}
+
+var YouTubeChannel = YouTubeChannelIdentity{
+	ID:     "UC1cnByKe24JjTv38tH_7BYw",
+	Handle: "@izuho_omi",
+}
