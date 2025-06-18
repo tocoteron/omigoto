fa2d59d1449aa8079ce0fe53efd41220618bda43
tocoteron <tocoteron@gmail.com>
2025-06-18 14:27:12 +0900

    CLI loads env vars

--- backend/cmd/cli/main.go
+++ backend/cmd/cli/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type config struct {
+	YouTubeAPIKey string `env:"YOUTUBE_API_KEY,notEmpty"`
+}
 
 func main() {
-	fmt.Println("omigoto CLI is running")
+	var cfg config
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("failed to parse config: %v", err)
+	}
+
+	fmt.Printf("%+v\n", cfg)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/tocoteron/omigoto/backend
 
 go 1.24.4
+
+require github.com/caarlos0/env/v11 v11.3.1
